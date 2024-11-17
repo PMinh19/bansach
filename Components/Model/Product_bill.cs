574ea8eff193e9be20075f361af4eb8ed0f57d40
PMinh19 <phammet915@gmail.com>
2024-11-18 01:10:26 +0700

    chỉnh lại hóa đơn, giỏ hàng

--- Components/Model/Product_bill.cs
+++ Components/Model/Product_bill.cs
@@ -2,12 +2,14 @@
 {
     public class Product_bill
     {
+        public int UserId { get; set; }
         public int ProductBillId { get; set; }
-        public int BillId { get; set; } 
+        public int BillId { get; set; }
         public int Price { get; set; }
         public int FeaturePId { get; set; }
-        public int Quatity { get; set; }
+        public int Quantity { get; set; }
         public DateTime Created { get; set; }
         public DateTime Updated { get; set; }
+        public List<Product_cart> ProductCarts { get; set; }
     }
 }
